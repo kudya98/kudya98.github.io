70a6e4c281ce152cff6027de8f7c71739d20524f
kudya98 <kuds4869@gmail.com>
2019-11-25 14:33:11 +0700

    Fix

---
+++ threejs-shooter/precache-manifest.90508a1261cb2c3886540b79b91abaf7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cff334b7b5ae88dab79bdded3c6ee9ca",
+    "url": "/threejs-shooter/index.html"
+  },
+  {
+    "revision": "237bb5a01e680fbc6afd",
+    "url": "/threejs-shooter/static/css/main.17a80f61.chunk.css"
+  },
+  {
+    "revision": "12a589aab2d6b7a2de53",
+    "url": "/threejs-shooter/static/js/2.f48eceb0.chunk.js"
+  },
+  {
+    "revision": "237bb5a01e680fbc6afd",
+    "url": "/threejs-shooter/static/js/main.6ec41349.chunk.js"
+  },
+  {
+    "revision": "92f5583d14997f2cd3ec",
+    "url": "/threejs-shooter/static/js/runtime-main.566a499f.js"
+  }
+]);
