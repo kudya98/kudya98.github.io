dd873e9c69f2d4eefac542c4faf6016320642f22
kudya98 <kuds4869@gmail.com>
2019-12-30 21:02:05 +0700

    Fix

---
+++ precache-manifest.cdd7c66df32be204f2d25646dc6f5079.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35fd444966b4dbc590f84d6720970dbe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d179f02723771e15887",
+    "url": "/static/css/main.5c80e229.chunk.css"
+  },
+  {
+    "revision": "e56839cb88ee925824bf",
+    "url": "/static/js/2.5a3229c6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.5a3229c6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5d179f02723771e15887",
+    "url": "/static/js/main.055d5047.chunk.js"
+  },
+  {
+    "revision": "b9821db3055a97b4abdf",
+    "url": "/static/js/runtime-main.b777d170.js"
+  }
+]);
